771c5461018aeb331f89a4ab2f710daeb5767c52
assan1997 <49047175+assan1997@users.noreply.github.com>
2022-01-24 17:34:55 +0000

    Cest encore chaud

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
-fetch("https://coronavirusapifr.herokuapp.com/data/live/france")
+// fetch("https://coronavirusapifr.herokuapp.com/data/live/paris")
+// .then((response) => response.json())
+// .then((data) => console.log(data))
+
+
+let apikey = 'dd864602b809434c9b1a9b3a3c20192d';
+
+
+// fetch(`https://api.covidactnow.org/v2/states.timeseries.json?apiKey=${apikey}`)
+// .then((response) => response.json())
+// .then((data) => console.log(data))
+fetch("https://api.covid19api.com/summary")
 .then((response) => response.json())
-.then((data) => console.log(data))
+.then((data) => {
+    const pays = data.Countries;
+    console.log(pays);
+    const tPays= [];
+    for (let i = 0; i < pays.length; i++) {
+        const element = pays[i];
+        console.log(element.TotalConfirmed);
+        if (element.Country = ) {
+            
+        }
+    }
+})
+
